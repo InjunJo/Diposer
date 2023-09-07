7367b108c8f77a94cd82a9603ca0c628a865a01d
Ryan J <107255371+InjunJo@users.noreply.github.com>
2023-09-07 09:08:32 +0900

    initial Commit

---
+++ src/main/java/team/moebius/disposer/DisposerApplication.java
@@ -0,0 +1,13 @@
+package team.moebius.disposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisposerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/moebius/disposer/DisposerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.moebius.disposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
